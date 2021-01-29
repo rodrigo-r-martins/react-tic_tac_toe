9a463a741903237b46f0848516631c0950dd01f1
rmartins85 <50893308+rmartins85@users.noreply.github.com>
2021-01-29 15:40:11 -0500

    making app responsive

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,71 +4,69 @@ import Square from './Square';
 import { calculateWinner } from '../calculateWinner';
 
 const Board = () => {
-    // Setting states
-    const [boardSquares, setBoardSquares] = useState([Array(9).fill(null)]);
-    const [xIsNext, setXisNext] = useState(true);
-    const winner = calculateWinner(boardSquares);
-    
-    // Handling the click on squares
-    const clickingSquare = index => {
-        const squares = [...boardSquares];
-        if (winner || squares[index]) return;
-        squares[index] = xIsNext ? 'X' : 'O';
-        setBoardSquares(squares);
-        setXisNext(!xIsNext);
-    }
+	// Setting states
+	const [boardSquares, setBoardSquares] = useState([Array(9).fill(null)]);
+	const [xIsNext, setXisNext] = useState(true);
+	const winner = calculateWinner(boardSquares);
+	
+	// Handling the click on squares
+	const clickingSquare = index => {
+		const squares = [...boardSquares];
+		if (winner || squares[index]) return;
+		squares[index] = xIsNext ? 'X' : 'O';
+		setBoardSquares(squares);
+		setXisNext(!xIsNext);
+	}
 
-    // Creating squares where users can choose
-    const creatingSquare = index => {
-        return (
-            <Square 
-                value={ boardSquares[index] } 
-                onClick={ () => clickingSquare(index) }
-            />
-        );
-    }
+	// Creating squares where users can choose
+	const creatingSquare = index => {
+		return (
+			<Square 
+				value={ boardSquares[index] } 
+				onClick={ () => clickingSquare(index) }
+			/>
+		);
+	}
 
-    // Restarting states so the game is restarted
-    const restartingGame = () => {
-        setBoardSquares([Array(9).fill(null)])
-        setXisNext(true)
-    }
-    
-    // Status shown on top of squares
-    let status = winner 
-        ? `Winner: ${winner}` 
-        : `Next player: ${xIsNext ? 'X' : 'O'}`;
-    
-    return (
-        <div className='app__board'>
-            <div className='app__status'>
-                { status }
-            </div>
-            <div className='board__squares'>
-                { creatingSquare(1) }
-                { creatingSquare(2) }
-                { creatingSquare(3) }
-            </div>
-            <div className='board__squares'>
-                { creatingSquare(4) }
-                { creatingSquare(5) }
-                { creatingSquare(6) }
-            </div>
-            <div className='board__squares'>
-                { creatingSquare(7) }
-                { creatingSquare(8) }
-                { creatingSquare(9) }
-            </div>
-            <div>
-                <button 
-                    className='restart__button'
-                    onClick={ () => restartingGame() }
-                >
-                    Restart
-                </button>
-            </div>
-        </div>
-    );
+	// Restarting states so the game is restarted
+	const restartingGame = () => {
+		setBoardSquares([Array(9).fill(null)])
+		setXisNext(true)
+	}
+	
+	// Status shown on top of squares
+	let status = winner 
+		? `Winner: ${winner}` 
+		: `Next player: ${xIsNext ? 'X' : 'O'}`;
+	
+	return (
+		<div className='app__board'>
+			<div className='app__status'>
+				{ status }
+			</div>
+			<div className='board__squares'>
+				{ creatingSquare(1) }
+				{ creatingSquare(2) }
+				{ creatingSquare(3) }
+			</div>
+			<div className='board__squares'>
+				{ creatingSquare(4) }
+				{ creatingSquare(5) }
+				{ creatingSquare(6) }
+			</div>
+			<div className='board__squares'>
+				{ creatingSquare(7) }
+				{ creatingSquare(8) }
+				{ creatingSquare(9) }
+			</div>
+			<button 
+				className='restart__button'
+				onClick={ () => restartingGame() }
+			>
+				Restart
+			</button>
+		</div>
+	);
 };
 
 export default Board;
